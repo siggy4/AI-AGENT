b46edb4a68927991eddd259347a6af51f8e8540d
monroe100 <michelle1hunnid@gmail.com>
2026-02-23 08:52:36 +0300

    Update project files

---
+++ scraper/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Partnership
+
+class PartnershipForm(forms.ModelForm):
+    class Meta:
+        model = Partnership
+        fields = [
+            'partner_name',
+            'company',
+            'country',
+            'point_of_contact',
+            'partnership_type',
+            'start_date',
+            'status',
+            
+
+        ]
+        
+        widgets = {
+            'start_date': forms.DateInput(attrs={'type': 'date'}),
+            'partnership_type': forms.Select(),
+            'status': forms.Select(),
+        }
