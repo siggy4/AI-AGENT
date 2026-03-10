0d338218c040e07b58ab816b10752108524f3fac
siggy4 <lwanga.siganga@strathmore.edu>
2026-03-10 17:54:33 +0300

    Accepted incoming changes

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -3,29 +3,12 @@ from firecrawl import FirecrawlApp
 from pydantic import BaseModel
 from typing import List
 from .models import Opportunity
-<<<<<<< HEAD
-from firecrawl import Firecrawl
-#from django.db import models
-=======
->>>>>>> asc
 
 firecrawl = FirecrawlApp(api_key="fc-0d3f0027ab614353a6256aefc731844a")
 
 
 # 1. Define the structure you want Firecrawl to find
 class TenderSchema(BaseModel):
-<<<<<<< HEAD
-    id: int | None = None
-    source_name: str
-    title: str
-    url: str
-    category: str | None = None
-    country: str | None = None
-    active: bool = True
-    deadline: str | None = None
-    posted_date: str | None = None
-    scraped_at: str | None = None
-=======
     source_name: str
     title: str
     url: str
@@ -34,21 +17,14 @@ class TenderSchema(BaseModel):
     active: bool = True
     deadline: str = ""
     posted_date: str = ""
->>>>>>> asc
     analyzed: bool = False
 
 
 class TenderList(BaseModel):
     tenders: List[TenderSchema]
 
-<<<<<<< HEAD
-def run_scraper():
-    doc = firecrawl.scrape("https://tenders.go.ke/tenders", formats=["markdown", "html"])
-    print(doc)
-=======
 
 def run_firecrawl_scraper():
->>>>>>> asc
     target_url = "https://tenders.go.ke/tenders"
 
     # 2. Tell Firecrawl to scrape and extract based on your schema
--- scraper/urls.py
+++ scraper/urls.py
@@ -3,12 +3,7 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from rest_framework.routers import DefaultRouter
 from .views import opportunities_page, run_scraper_api, home, dashboard_page, new_partnership_page, about_page, \
-<<<<<<< HEAD
-    partnerships_list, opportunities_list
-from .views import delete_selected_partnerships
-=======
     partnerships_list, opportunities_list, new_opportunities, add_interest, remove_interest, update_interest
->>>>>>> asc
 
 from scraper import views
 from scraper.views import OpportunityViewSet
@@ -17,19 +12,14 @@ router = DefaultRouter()
 router.register(r'opportunities', OpportunityViewSet, basename='opportunity')
 
 urlpatterns = [
-    path('api/partnerships/', views.create_partnership_api, name='create_partnership_api'),
     path('api/', include(router.urls)),
     path('run-scraper/', run_scraper_api, name='run_scraper_api'),
     path('', home, name='home'),
-  
 
     path('dashboard/', dashboard_page, name='dashboard_page'),
     path('new/', new_partnership_page, name='new_partnership'),
     path('list/', partnerships_list, name='partnerships'),
-    path('delete-selected/', views.delete_selected_partnerships, name='delete_selected_partnerships'),
     path('update_partnership/<int:pk>/', views.update_partnership, name='update_partnership'),
-    path('update-partnership-details/<int:pk>/', views.update_partnership_details, name='update_partnership_details'),
- 
 
     # opportunity detail view
     path('opportunities/', opportunities_page, name='opportunities_page'),
@@ -45,9 +35,4 @@ urlpatterns = [
     path('about/', about_page, name='about'),
     path('login/', auth_views.LoginView.as_view(template_name='scraper/home.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
-
-    path('manual/', views.manual_partnerships, name='manual_partnerships'),
-    path('edit/<int:pk>/', views.edit_partnership, name='edit_partnership'),
-    
-   
 ]
